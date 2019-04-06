6b8bb55ad121fe2ba9cfb9bb5445a691aca26587
Mark Hernandez <m.hern1415@gmail.com>
2019-04-06 00:17:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,4 +42,12 @@ def turn_count(board)
   return turn
 end
 
-
+def current_player(board)
+  num_turns = turn_count(board)
+  if num_turns % 2 == 0
+    player = "X"
+  else
+    player = "O"
+  end
+  return player
+end
